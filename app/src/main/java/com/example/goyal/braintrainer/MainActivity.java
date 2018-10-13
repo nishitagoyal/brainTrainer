6c57d6909c736eb17ddfc6b9dad4d801175b610a
nishita goyal <goyalnishita07@gmail.com>
2018-10-13 13:39:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/goyal/braintrainer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.goyal.braintrainer;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            }
+        },2000);
+    }
+}
